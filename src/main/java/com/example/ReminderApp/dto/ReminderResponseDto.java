7a3120eefc03f2e1649686d9dcb4804d7289d313
AlexKomarov93 <alexfurmanov30@gmail.com>
2026-04-22 12:08:23 +0300

    .

--- src/main/java/com/example/ReminderApp/dto/ReminderResponseDto.java
+++
@@ -1,9 +0,0 @@
-package com.example.ReminderApp.dto;
-
-import java.time.LocalDateTime;
-
-public record ReminderResponseDto(
-        String title,
-        String description,
-        LocalDateTime remind) {
-}
